c292609621460091b76e8f31b9d896a8eb71f58a
Dwuangne <tquang0809@gmail.com>
2024-05-19 17:11:31 +0700

    text

---
+++ mamababy/src/main/java/com/myweb/mamababy/MamababyApplication.java
@@ -0,0 +1,13 @@
+package com.myweb.mamababy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamababyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamababyApplication.class, args);
+	}
+
+}
