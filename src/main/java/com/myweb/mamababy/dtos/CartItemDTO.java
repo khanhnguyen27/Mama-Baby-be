59d952194c8f8d43db41f2c91ec8cf353cca1e5c
DangKhoi <kwoitd03@gmail.com>
2024-06-01 16:42:54 +0700

    add to cart

---
+++ src/main/java/com/myweb/mamababy/dtos/CartItemDTO.java
@@ -0,0 +1,20 @@
+package com.myweb.mamababy.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartItemDTO {
+
+    @JsonProperty("product_id")
+    private int productId;
+
+    @JsonProperty("quantity")
+    private int quantity;
+}
